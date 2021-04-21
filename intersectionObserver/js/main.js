852b6df71f347de8d2fc0577aa05b2edd4d12d99
kenji <kenjix12x22@gmail.com>
2021-04-21 20:50:49 +0900

    observer8

---
+++ intersectionObserver/js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+{
+  const target = document.querySelector('img');
+
+  function callback(entries) {
+    console.log(entries[0]);
+
+    if(entries[0].isIntersecting){
+      entries[0].target.classList.add('appear');
+    }else{
+      entries[0].target.classList.remove('appear');
+    }
+  };
+
+  const options = {
+    threshold: 0.2, 
+  }
+
+  const observer = new IntersectionObserver(callback, options);
+
+  observer.observe(target);
+
+}
